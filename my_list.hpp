30a2b39c5e9bbef4db5c5640648be7ff98491d95
Filippov Dmytro <dmytro.filippov27@gmail.com>
2021-05-07 22:01:43 +0300

    add list with unique_ptr

--- my_list.hpp
+++ my_list.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <cassert>
 #include <iostream>
 #include <memory>
 
-template <typename T> class list {
+template <typename T> class List {
   struct node {
     T data{};
     std::unique_ptr<node> next;
     node *prev{};
 
     node() = default;
-    node(int data_) : data{data_}, next{nullptr}, prev{nullptr} {}
+    explicit node(const T &data_) : data{data_}, next{nullptr}, prev{nullptr} {}
+    explicit node(T &&data_)
+        : data{std::move(data_)}, next{nullptr}, prev{nullptr} {}
   };
 
   std::unique_ptr<node> head;
@@ -23,21 +26,93 @@ public:
   using reference = T &;
   using const_reference = const T &;
 
-  iterator begin() { return head; }
+  iterator begin() { return head.get(); }
   iterator end() { return current; }
-  const_iterator begin() const { return head; }
+  const_iterator begin() const { return head.get(); }
   const_iterator end() const { return current; }
 
   size_t size() const { return size_; }
+  reference front() { return head->data; }
+  const_reference front() const { return head->data; }
+  reference back() { return current->data; }
+  const_reference back() const { return current->data; }
+  bool empty() const { return head == nullptr; }
 
-  void push_front(T value) {
+  void push_front(const T &value) {
     auto tmpNode = std::make_unique<node>(value);
-    tmpNode->data = value;
-    tmpNode->next = std::move(head);
     if (head == nullptr) {
-      current = std::move(tmpNode);
-      head = tmpNode.get();
+      head = std::move(tmpNode);
+      current = head.get();
+    } else {
+      head->prev = tmpNode.get();
+      tmpNode->next = std::move(head);
+      head = std::move(tmpNode);
     }
-    head = tmpNode.get();
+    ++size_;
+  }
+
+  void push_front(T &&value) {
+    auto tmpNode = std::make_unique<node>(std::move(value));
+    if (head == nullptr) {
+      head = std::move(tmpNode);
+      current = head.get();
+    } else {
+      head->prev = tmpNode.get();
+      tmpNode->next = std::move(head);
+      head = std::move(tmpNode);
+    }
+    ++size_;
+  }
+
+  void push_back(const T &value) {
+    auto tmpNode = std::make_unique<node>(value);
+    if (head == nullptr) {
+      head = std::move(tmpNode);
+      current = head.get();
+    } else {
+      tmpNode->prev = current;
+      current->next = std::move(tmpNode);
+      current = (current->next).get();
+    }
+    ++size_;
+  }
+
+  void push_back(T &&value) {
+    auto tmpNode = std::make_unique<node>(std::move(value));
+    if (head == nullptr) {
+      head = std::move(tmpNode);
+      current = head.get();
+    } else {
+      tmpNode->prev = current;
+      current->next = std::move(tmpNode);
+      current = (current->next).get();
+    }
+    ++size_;
+  }
+
+  template <typename... Args> void emplace_back(Args &&...args) {
+    auto tmpNode = std::make_unique<node>(T{std::forward<Args>(args)...});
+    if (head == nullptr) {
+      head = std::move(tmpNode);
+      current = head.get();
+    } else {
+      tmpNode->prev = current;
+      current->next = std::move(tmpNode);
+      current = (current->next).get();
+    }
+    ++size_;
+  }
+
+  void pop_back() {
+    assert(!empty());
+    if (head.get() == current) {
+      head.release();
+      delete current;
+    } else {
+      current = current->prev;
+      auto tmpPtr = (current->next).release();
+      delete tmpPtr;
+    }
+    --size_;
   }
 };
--- testList.cpp
+++ testList.cpp
@@ -1,24 +1,99 @@
 #include "my_list.hpp"
 
 #include <iostream>
+#include <list>
 
 #include "catch2/catch.hpp"
 
 TEST_CASE("test list") {
 
-  list<int> list;
+  List<int> list;
 
   CHECK(list.begin() == nullptr);
   CHECK(list.end() == nullptr);
   CHECK(list.size() == 0);
+  CHECK(list.empty() == true);
 
-  list.push_front(3);
-  list.push_front(4);
   list.push_front(5);
+  list.push_front(4);
+  list.push_front(3);
 
-  CHECK(list.begin()->data == 5);
-  CHECK(list.begin()->next->data == 4);
-  CHECK(list.end()->data == 3);
-  CHECK(list.end()->prev->data == 4);
-  CHECK(list.size() == 3);
+  CHECK(list.front() == 3);
+  CHECK(list.back() == 5);
+  CHECK(list.empty() == false);
+
+  SECTION("testing push_front") {
+
+    CHECK(list.begin()->data == 3);
+    CHECK(list.begin()->next->data == 4);
+    CHECK(list.end()->data == 5);
+    CHECK(list.end()->prev->data == 4);
+    CHECK(list.size() == 3);
+  }
+
+  SECTION("testing push_back") {
+
+    list.push_back(20);
+    list.push_back(30);
+
+    CHECK(list.end()->data == 30);
+    CHECK(list.end()->prev->data == 20);
+    CHECK(list.size() == 5);
+
+    List<int> list2;
+
+    list2.push_back(25);
+    list2.push_back(35);
+
+    CHECK(list2.begin()->data == 25);
+    CHECK(list2.begin()->next->data == 35);
+    CHECK(list2.end()->data == 35);
+  }
+
+  SECTION("testing pop_back") {
+
+    list.pop_back();
+
+    CHECK(list.back() == 4);
+    CHECK(list.size() == 2);
+
+    list.pop_back();
+
+    CHECK(list.back() == 3);
+    CHECK(list.size() == 1);
+
+    list.pop_back();
+
+    CHECK(list.empty() == true);
+    CHECK(list.size() == 0);
+  }
+}
+
+struct Test {
+  int value{};
+
+  Test() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+  Test(int value_) : value(value_) {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+  Test(const Test &rhs) : value(rhs.value) {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+  Test(Test &&rhs) : value(std::move(rhs.value)) {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+};
+
+TEST_CASE("testing emplace") {
+
+  List<Test> list3;
+
+  list3.emplace_back(4);
+  list3.emplace_back(44);
+  list3.emplace_back(55);
+
+  CHECK(list3.begin()->data.value == 4);
+  CHECK(list3.begin()->next->data.value == 44);
+  CHECK(list3.end()->data.value == 55);
+  CHECK(list3.end()->prev->data.value == 44);
 }
